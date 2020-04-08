c56dc759e08cad760eace327cc9ea92ded40991c
Masha Budryte <masha.budryte@thoughtworks.com>
2020-04-08 09:09:37 +0100

    Adding props to the person: name and age

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
       <div className="App">
         <h1> Hello, home! I am a React App!</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Masha" age="25" />
+        <Person name="Barsik" age="15">
+          I like sleeping
+        </Person>
+        <Person name="Olga" age="55" />
       </div>
     );
     // return React.createElement(
