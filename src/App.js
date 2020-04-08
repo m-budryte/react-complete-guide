e7eda4f2c8fcf62f3599b39aa26f001428436476
Masha Budryte <masha.budryte@thoughtworks.com>
2020-04-08 08:45:44 +0100

    Use React.createElement

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1> Hello, home! I am a React App!</h1>
+        <p>This is really working!</p>
       </div>
     );
+    // return React.createElement(
+    //   "div",
+    //   null,
+    //   React.createElement("h1", null, "Does this work now?")
+    // );
   }
 }
 
