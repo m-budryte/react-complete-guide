5f4f64ba72811f611e315ad51d29f81c281c36df
Masha Budryte <masha.budryte@thoughtworks.com>
2020-04-09 18:42:14 +0100

    revert correctly back to the SetState class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const app = (props) => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: "Masha", age: 25 },
       { name: "Barsik", age: 15 },
@@ -24,32 +24,34 @@ const app = (props) => {
     });
   };
 
-  return (
-    <div className="App">
-      <h1> Hello, home! I am a React App!</h1>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      />
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-      >
-        I like sleeping
-      </Person>
-      <Person
-        name={personsState.persons[2].name}
-        age={personsState.persons[2].age}
-      />
-    </div>
-  );
-  // return React.createElement(
-  //   "div",
-  //   null,
-  //   React.createElement("h1", null, "Does this work now?")
-  // );
-};
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello, home! I am a React App!</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          I like sleeping
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+    // return React.createElement(
+    //   "div",
+    //   null,
+    //   React.createElement("h1", null, "Does this work now?")
+    // );
+  }
+}
 
-export default app;
+export default App;
