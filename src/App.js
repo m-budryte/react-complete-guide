2ae0e027f300eb06d9a9f4a1c6b885d3e3036929
Masha Budryte <masha.budryte@thoughtworks.com>
2020-04-10 16:19:08 +0100

    56. Outputting Lists
    Outputing the list by mapping the array into the array of ES6 elements

--- src/App.js
+++ src/App.js
@@ -54,23 +54,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-            changed={this.nameChangedHandler}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Barsik!")}
-            // use this rather than the example in <button>
-          >
-            I like sleeping
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person) => {
+            return <Person name={person.name} age={person.age} />;
+          })}
         </div>
       );
     }
