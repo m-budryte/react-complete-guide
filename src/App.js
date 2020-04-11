7f962411f7bb07267e5926bda608a19d1ea8fd62
Masha Budryte <masha.budryte@thoughtworks.com>
2020-04-11 11:55:22 +0100

    60. Flexible Lists
    nameChangedHandler is updated to work with copies of the state rather than directly mutating it

--- src/App.js
+++ src/App.js
@@ -22,14 +22,21 @@ class App extends Component {
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
   };
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: event.target.value, age: 25 },
-        { name: "Barsik", age: 16 },
-        { name: "Olga", age: 55 },
-      ],
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
     });
+
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -58,6 +65,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
