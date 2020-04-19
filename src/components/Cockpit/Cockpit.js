98d160de63644919b121f571491c3f91741e7b05
Masha <mashabudryte@gmail.com>
2020-04-19 15:13:27 +0100

    94. Controlling the useEffect() Behavior

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -4,7 +4,10 @@ import classes from './Cockpit.css'
 const cockpit = (props) => {
     useEffect(() => {
         console.log('[Cockpit.js] useEffect')
-    })
+        setTimeout(()=> {
+            alert('Saved to the cloud')
+        }, 1000)
+    }, [])
 
     let assignedClasses = [];
     let btnClass = '';
