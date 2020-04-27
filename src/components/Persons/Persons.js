06a4102d4d1468eb449f694cffff07e097872a11
Masha Budryte <masha.budryte@thoughtworks.com>
2020-04-27 08:44:58 +0100

    97. Using shouldComponentUpdate for Optimization

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,48 +1,51 @@
-import React, { Component } from 'react';
-import Person from './Person/Person'
+import React, { Component } from "react";
+import Person from "./Person/Person";
 
 class Persons extends Component {
-
   // static getDerivedStateFromProps() {
   //   console.log('[Persons.js] getDerivedStateFromProps');
   // }
 
-  componentWillReceiveProps(props) {
-    console.log('[Persons.js] componentWillReceiveProps', props)
-  }
+  // componentWillReceiveProps(props) {
+  //   console.log("[Persons.js] componentWillReceiveProps", props);
+  // }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('[Persons.js] shouldComponentUpdate');
-    return true;
+    console.log("[Persons.js] shouldComponentUpdate");
+    if (nextProps.persons !== this.props.persons) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('[Persons.js] getSnapshotBeforeUpdate');
-    return { message: 'Snapshot!'};
+    console.log("[Persons.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('[Person.js] componentDidUpdate');
+    console.log("[Person.js] componentDidUpdate");
     console.log(snapshot);
   }
 
   componentWillUnmount() {
-    console.log('[Person.js] componentWillUnmount')
+    console.log("[Person.js] componentWillUnmount");
   }
-  render() {  
-    
-    console.log('[Persons.js] rendering...');
+  render() {
+    console.log("[Persons.js] rendering...");
     return this.props.persons.map((person, index) => {
-        return (
-          <Person
-            click={() => this.props.clicked(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.props.changed(event, person.id)}
-          />
-        );
-  })}}
-
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={(event) => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
+}
 
 export default Persons;
