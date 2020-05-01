13897176bfa63e652ea7f80dfeb9ac0b8e13aca4
Masha <mashabudryte@gmail.com>
2020-05-01 20:22:30 +0100

    113. Using the Context API

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import Cockpit from "../components/Cockpit/Cockpit";
 import Persons from "../components/Persons/Persons";
 import withClass from "../hoc/withClass";
 import Aux from "../hoc/Aux";
+import AuthContext from '../context/auth-context'
 
 class App extends Component {
   constructor(props) {
@@ -110,16 +111,21 @@ class App extends Component {
         >
           Remove cockpit
         </button>
-        {this.state.showCockpit ? (
-          <Cockpit
-            title={this.props.appTitle}
-            showPersons={this.state.showPersons}
-            personsLength={this.state.persons.length}
-            clicked={this.togglePersonsHandler}
-            login={this.loginHandler}
-          />
-        ) : null}
-        {persons}
+        <AuthContext.Provider value={{
+          authenticated: this.state.authenticated,
+          login: this.loginHandler
+        }}>
+          {this.state.showCockpit ? (
+            <Cockpit
+              title={this.props.appTitle}
+              showPersons={this.state.showPersons}
+              personsLength={this.state.persons.length}
+              clicked={this.togglePersonsHandler}
+              login={this.loginHandler}
+            />
+          ) : null}
+          {persons}
+        </AuthContext.Provider>
       </Aux>
     );
   }
